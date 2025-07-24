ad403d081a04f5015fe9e256763d0db181276772
taufiqzx9 <tnssannu@gmail.com>
2025-07-24 18:53:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+function logout() {
+  window.location.href = "index.html"; // Or "login.html"
+}
 document.addEventListener("DOMContentLoaded", () => {
   const tableBody = document.getElementById("stationTableBody");
   const policeStations = [
@@ -67,9 +70,7 @@ document.addEventListener("DOMContentLoaded", () => {
 ["SDPO बारामती कार्यालय", "अतुल जाधव", "8668510084", "dybaramatido", ""]
 
 ];
-  function logout() {
-  window.location.href = "index.html"; // Or "login.html"
-}
+  
 
  policeStations.forEach((stationInfo, index) => {
     const [station, officer, mobile, username, landline] = stationInfo;
